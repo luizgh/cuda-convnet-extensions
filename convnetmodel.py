91efd0f7ad819de859d7613150bc5747e44ca65b
Luiz Gustavo Hafemann <lghafemann@inf.ufpr.br>
2014-05-26 22:30:58 -0300

    x

--- convnetmodel.py
+++ convnetmodel.py
@@ -3,12 +3,13 @@ from mock import Mock, PropertyMock, MagicMock
 import cPickle
 
 class ConvnetModel:
-    def __init__(self, layer_def_file, layer_params_file, train_provider):
+    def __init__(self, layer_def_file, layer_params_file, train_provider, verbose = False):
         self.layer_def = layer_def_file
         self.layer_params = layer_params_file
         self.minibatch_size = 128
         self.gpu_device_id = 0
         self.train_provider = train_provider
+        self.verbose = verbose
         self.layers = self._BuildLayers()
         self._InitializeModel()
         self.train_results =[]
@@ -16,12 +17,6 @@ class ConvnetModel:
     def _BuildLayers(self):
         fakeModel = Mock()
         myOp = Mock()
-        '''
-        fakeDataProvider = Mock()
-        fakeDataProvider.get_data_dims = MagicMock(side_effect = lambda idx: inputSize if idx == 0 else 1)
-        fakeDataProvider.get_num_classes.return_value = numClasses
-        type(fakeModel).train_data_provider = PropertyMock(return_value = fakeDataProvider)
-        '''
         myOp.get_value.return_value = 0
         type(fakeModel).train_data_provider = self.train_provider
         type(fakeModel).op = PropertyMock(return_value = myOp)
@@ -34,6 +29,8 @@ class ConvnetModel:
     def Run(self, nIterations):
         next_data = self.train_provider.get_next_batch()
         for i in xrange(nIterations):
+            if self.verbose:
+                print("Running iteration %d of %d" % (i, nIterations))
             data = next_data
             self.libmodel.startBatch(data[2], False)
             next_data = self.train_provider.get_next_batch()
